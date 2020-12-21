550f77e9062d0dc8bd86a0481b520d1e6b0c89aa
Nasan01 <avonasandratra2003@gmail.com>
2020-12-21 07:14:25 +0300

    about emploi du temps

--- App/Table/TableReservation.php
+++ App/Table/TableReservation.php
@@ -16,4 +16,26 @@ class TableReservation
         return $data;
     }
 
+    public static function getWithLink($debut, $fin){
+        $db = new Database('reservationTennis');
+        $data = $db->query(
+            "SELECT r.date_reservation reservationDate, d.plage_horaire horairePlage
+                      FROM reservation r 
+                      LEFT JOIN date_heure d
+                      ON r.heure_reservation = d.id_dateHeure
+                      LIMIT ".$debut.", ".$fin,
+        'App\Table\TableReservation'
+        );
+        return $data;
+    }
+
+    public static function getTest($debut, $fin){
+        $db = new Database('reservationTennis');
+        $data = $db->query(
+            "SELECT * FROM test LIMIT ".$debut.", ".$fin,
+            "App\Table\TableReservation"
+        );
+        return $data;
+    }
+
 }
--- page/nonAdherents/reservation.php
+++ page/nonAdherents/reservation.php
@@ -14,27 +14,40 @@
     <div class="col-md-9">
         <div class="container-fluid">
             <div class="row">
-                <table class="table-bordered">
-                    <?php
-                    function convertDate($dateAConverte){
-                        $tabDate = explode('-', $dateAConverte);
-                        $jour = (int) $tabDate[2];
-                        $mois = (int) $tabDate[1];
-                        $annee = (int) $tabDate[0];
-                        $dateTimeStamp = mktime(0, 0, 0, $mois, $jour, $annee);
-                        $dateConverti = date('D-d-M-Y', $dateTimeStamp);
-                        return $dateConverti;
-                    }
-                    $test = \App\Table\TableDateHeure::getTime();
-                    $tabDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-                    ?>
+                <?php
+                function convertDate($dateAConverte){
+                    $tabDate = explode('-', $dateAConverte);
+                    $jour = (int) $tabDate[2];
+                    $mois = (int) $tabDate[1];
+                    $annee = (int) $tabDate[0];
+                    $dateTimeStamp = mktime(0, 0, 0, $mois, $jour, $annee);
+                    $dateConverti = date('D-d-M-Y', $dateTimeStamp);
+                    return $dateConverti;
+                }
+                $test = \App\Table\TableDateHeure::getTime();
+                $tabDay = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+                ?>
+
+            <div class="test">
+                <table class="table" border="1">
                     <tr>
-                        <td>Plage Horaire / Jour</td>
+                        <td>jour/heure</td>
+                        <td>8h-10h</td>
+                        <td>10h-12h</td>
+                        <td>12h-14h</td>
+                        <td>14h-16h</td>
+                        <td>16h-18h</td>
+                        <td>18h-20h</td>
                     </tr>
-                    <?php foreach (\App\Table\TableDateHeure::getAll() as $value):?>
+                    <?php   foreach (\App\Table\TableReservation::getTest(0, 7) as $test): ?>
                         <tr>
-                            <td><?= $value->horaire;?></td>
-                            <td><?= $value->nbrReservation;?> courts libre(s)</td>
+                            <td><?= convertDate($test->date);?></td>
+                            <td><?= $test->nbr_8_10;?> court(s) libre(s)</td>
+                            <td><?= $test->nbr_10_12;?> court(s) libre(s)</td>
+                            <td><?= $test->nbr_12_14;?> court(s) libre(s)</td>
+                            <td><?= $test->nbr_14_16;?> court(s) libre(s)</td>
+                            <td><?= $test->nbr_16_18;?> court(s) libre(s)</td>
+                            <td><?= $test->nbr_18_20;?> court(s) libre(s)</td>
                         </tr>
                     <?php endforeach;?>
                 </table>
